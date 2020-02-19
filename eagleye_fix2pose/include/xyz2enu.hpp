29a0892aa5954b020f4f2ba9f4caff95ccf81649
Aoki-Takanose <49303804+Aoki-Takanose@users.noreply.github.com>
2020-02-19 14:48:56 +0900

    Corresponds to geoid height
    Solved the problem that the height could not be output at the elevation in the MMS point cloud.

---
+++ eagleye_fix2pose/include/xyz2enu.hpp
@@ -0,0 +1,68 @@
+// Copyright (c) 2019, Map IV, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of the Map IV, Inc. nor the names of its contributors
+//   may be used to endorse or promote products derived from this software
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+void xyz2enu(double ecef_pos[3], double ecef_base_pos[3], double enu_pos[0])
+{
+  //ecef2llh
+  double semi_major_axis = 6378137.0000;
+  double semi_minor_axis = 6356752.3142;
+  double a1 = sqrt(1 - pow(semi_minor_axis / semi_major_axis, 2.0));
+  double a2 = sqrt((ecef_base_pos[0] * ecef_base_pos[0]) + (ecef_base_pos[1] * ecef_base_pos[1]));
+  double a3 = 54 * (semi_minor_axis * semi_minor_axis) * (ecef_base_pos[2] * ecef_base_pos[2]);
+  double a4 = (a2 * a2) + (1 - (a1 * a1)) * (ecef_base_pos[2] * ecef_base_pos[2]) - (a1 * a1) * (semi_major_axis * semi_major_axis - semi_minor_axis * semi_minor_axis);
+  double a5 = ((a1 * a1) * (a1 * a1) * a3 * (a2 * a2)) / (a4 * a4 * a4);
+  double a6 = pow((1 + a5 + sqrt(a5 * a5 + 2 * a5)), 1.0 / 3.0);
+  double a7 = a3 / (3 * pow((a6 + 1 / a6 + 1), 2.0) * a4 * a4);
+  double a8 = sqrt(1 + 2 * (a1 * a1) * (a1 * a1) * a7);
+  double a9 = -(a7 * (a1 * a1) * a2) / (1 + a8) + sqrt((semi_major_axis * semi_major_axis / 2) * (1 + 1 / a8) - (a7 * (1 - (a1 * a1)) * (ecef_base_pos[2] * ecef_base_pos[2])) / (a8 * (1 + a8)) - a7 * (a2 * a2) / 2);
+  double a10 = sqrt((pow((a2 - (a1 * a1) * a9), 2.0)) + (ecef_base_pos[2] * ecef_base_pos[2]));
+  double a11 = sqrt((pow((a2 - (a1 * a1) * a9), 2.0)) + (1 - (a1 * a1)) * (ecef_base_pos[2] * ecef_base_pos[2]));
+  double a12 = ((semi_minor_axis * semi_minor_axis) * ecef_base_pos[2]) / (semi_major_axis * a11);
+  double base_latitude = atan((ecef_base_pos[2] + (a1 * (semi_major_axis / semi_minor_axis)) * (a1 * (semi_major_axis / semi_minor_axis)) * a12) / a2);
+  double base_longitude = 0;
+
+  if (ecef_base_pos[0] >= 0)
+  {
+    base_longitude = (atan(ecef_base_pos[1] / ecef_base_pos[0]));
+  }
+  else
+  {
+    if (ecef_base_pos[0] < 0 && ecef_base_pos[1] >= 0)
+    {
+      base_longitude = M_PI + (atan(ecef_base_pos[1] / ecef_base_pos[0]));
+    }
+    else
+    {
+      base_longitude = (atan(ecef_base_pos[1] / ecef_base_pos[0])) - M_PI;
+    }
+  }
+
+  double base_alt = a10 * (1 - (semi_minor_axis * semi_minor_axis) / (semi_major_axis * a11));
+
+  //ecef2enu
+  enu_pos[0] = ((-(sin(base_longitude)) * (ecef_pos[0] - ecef_base_pos[0])) + ((cos(base_longitude)) * (ecef_pos[1] - ecef_base_pos[1])) + (0 * (ecef_pos[2] - ecef_base_pos[2])));
+  enu_pos[1] = ((-(sin(base_latitude)) * (cos(base_longitude)) * (ecef_pos[0] - ecef_base_pos[0])) + (-(sin(base_latitude)) * (sin(base_longitude)) * (ecef_pos[1] - ecef_base_pos[1])) + ((cos(base_latitude)) * (ecef_pos[2] - ecef_base_pos[2])));
+  enu_pos[2] = (((cos(base_latitude)) * (cos(base_longitude)) * (ecef_pos[0] - ecef_base_pos[0])) + ((cos(base_latitude)) * (sin(base_longitude)) * (ecef_pos[1] - ecef_base_pos[1])) + ((sin(base_latitude)) * (ecef_pos[2] - ecef_base_pos[2])));
+}
