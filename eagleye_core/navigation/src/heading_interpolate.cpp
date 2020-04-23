7f65edc4b0d4541270eb4357a3e49819d119b39c
OsamuSekino <44186078+OsamuSekino@users.noreply.github.com>
2020-04-23 14:00:35 +0900

    Revert "Feature/functionalization (#25)"
    This reverts commit 1ffb281533e5ef8d21f78cc053ce7547d0b94b61.

--- eagleye_core/navigation/src/heading_interpolate.cpp
+++ eagleye_core/navigation/src/heading_interpolate.cpp
@@ -24,31 +24,102 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /*
- * heading_interpolate->cpp
+ * heading_interpolate.cpp
  * Author MapIV Sekino
  */
 
-#include "navigation.hpp"
+#include "ros/ros.h"
+#include "eagleye_msgs/VelocityScaleFactor.h"
+#include "eagleye_msgs/Heading.h"
+#include "eagleye_msgs/YawrateOffset.h"
+#include "eagleye_msgs/SlipAngle.h"
+#include "geometry_msgs/TwistStamped.h"
+#include "sensor_msgs/Imu.h"
+#include <boost/circular_buffer.hpp>
+#include <math.h>
+#include <numeric>
 
-void heading_interpolate_estimate(const sensor_msgs::Imu imu, const eagleye_msgs::VelocityScaleFactor velocity_scale_factor, const eagleye_msgs::YawrateOffset yawrate_offset_stop,const eagleye_msgs::YawrateOffset yawrate_offset,const eagleye_msgs::Heading heading,const eagleye_msgs::SlipAngle slip_angle,const HeadingInterpolateParameter heading_interpolate_parameter, HeadingInterpolateStatus* heading_interpolate_status,eagleye_msgs::Heading* heading_interpolate)
+//default value
+static bool reverse_imu = false;
+static double stop_judgment_velocity_threshold = 0.01;
+static double number_buffer_max = 100;
+
+static bool heading_estimate_status, heading_estimate_start_status;
+static int i, count, heading_estimate_status_count;
+static int estimate_index = 0;
+static int number_buffer = 0;
+
+static double heading_stamp_last = 0;
+static double time_last = 0.0;
+static double yawrate = 0.0;
+static double provisional_heading_angle = 0.0;
+static double diff_estimate_heading_angle = 0.0;
+
+static boost::circular_buffer<double> provisional_heading_angle_buffer(number_buffer_max);
+static boost::circular_buffer<double> imu_stamp_buffer(number_buffer_max);
+
+static eagleye_msgs::VelocityScaleFactor velocity_scale_factor;
+static eagleye_msgs::YawrateOffset yawrate_offset_stop;
+static eagleye_msgs::YawrateOffset yawrate_offset;
+static eagleye_msgs::Heading heading;
+static eagleye_msgs::SlipAngle slip_angle;
+
+static ros::Publisher pub;
+static eagleye_msgs::Heading heading_interpolate;
+
+void velocity_scale_factor_callback(const eagleye_msgs::VelocityScaleFactor::ConstPtr& msg)
 {
-  int i;
-  int estimate_index = 0;
-  double yawrate = 0.0;
-  double diff_estimate_heading_angle = 0.0;
-  bool heading_estimate_status, heading_estimate_start_status;
-  std::size_t imu_stamp_buffer_length;
+  velocity_scale_factor.header = msg->header;
+  velocity_scale_factor.scale_factor = msg->scale_factor;
+  velocity_scale_factor.correction_velocity = msg->correction_velocity;
+  velocity_scale_factor.status = msg->status;
+}
 
-  if (heading_interpolate_parameter.reverse_imu == false)
+void yawrate_offset_stop_callback(const eagleye_msgs::YawrateOffset::ConstPtr& msg)
+{
+  yawrate_offset_stop.header = msg->header;
+  yawrate_offset_stop.yawrate_offset = msg->yawrate_offset;
+  yawrate_offset_stop.status = msg->status;
+}
+
+void yawrate_offset_callback(const eagleye_msgs::YawrateOffset::ConstPtr& msg)
+{
+  yawrate_offset.header = msg->header;
+  yawrate_offset.yawrate_offset = msg->yawrate_offset;
+  yawrate_offset.status = msg->status;
+}
+
+void heading_callback(const eagleye_msgs::Heading::ConstPtr& msg)
+{
+  heading.header = msg->header;
+  heading.heading_angle = msg->heading_angle;
+  heading.status = msg->status;
+}
+
+void slip_angle_callback(const eagleye_msgs::SlipAngle::ConstPtr& msg)
+{
+  slip_angle.header = msg->header;
+  slip_angle.coefficient = msg->coefficient;
+  slip_angle.slip_angle = msg->slip_angle;
+  slip_angle.status = msg->status;
+}
+
+void imu_callback(const sensor_msgs::Imu::ConstPtr& msg)
+{
+  heading_interpolate.header = msg->header;
+
+  ++count;
+
+  if (reverse_imu == false)
   {
-    yawrate = imu.angular_velocity.z;
+    yawrate = msg->angular_velocity.z;
   }
-  else if (heading_interpolate_parameter.reverse_imu == true)
+  else if (reverse_imu == true)
   {
-    yawrate = -1 * imu.angular_velocity.z;
+    yawrate = -1 * msg->angular_velocity.z;
   }
 
-  if (velocity_scale_factor.correction_velocity.linear.x > heading_interpolate_parameter.stop_judgment_velocity_threshold)
+  if (velocity_scale_factor.correction_velocity.linear.x > stop_judgment_velocity_threshold)
   {
     yawrate = yawrate + yawrate_offset.yawrate_offset;
   }
@@ -57,16 +128,16 @@ void heading_interpolate_estimate(const sensor_msgs::Imu imu, const eagleye_msgs
     yawrate = yawrate + yawrate_offset_stop.yawrate_offset;
   }
 
-  if (heading_interpolate_status->number_buffer < heading_interpolate_parameter.number_buffer_max)
+  if (number_buffer < number_buffer_max)
   {
-    ++heading_interpolate_status->number_buffer;
+    ++number_buffer;
   }
   else
   {
-    heading_interpolate_status->number_buffer = heading_interpolate_parameter.number_buffer_max;
+    number_buffer = number_buffer_max;
   }
 
-  if (heading_interpolate_status->heading_stamp_last == heading.header.stamp.toSec())
+  if (heading_stamp_last == heading.header.stamp.toSec())
   {
     heading_estimate_status = false;
   }
@@ -74,74 +145,139 @@ void heading_interpolate_estimate(const sensor_msgs::Imu imu, const eagleye_msgs
   {
     heading_estimate_status = true;
     heading_estimate_start_status = true;
-    ++heading_interpolate_status->heading_estimate_status_count;
+    ++heading_estimate_status_count;
   }
 
-  if(heading_interpolate_status->time_last != 0)
+  if(time_last != 0)
   {
-    heading_interpolate_status->provisional_heading_angle = heading_interpolate_status->provisional_heading_angle + (yawrate * (imu.header.stamp.toSec() - heading_interpolate_status->time_last));
+    provisional_heading_angle = provisional_heading_angle + (yawrate * (msg->header.stamp.toSec() - time_last));
   }
 
-  // data buffer generate
-  heading_interpolate_status->provisional_heading_angle_buffer.push_back(heading_interpolate_status->provisional_heading_angle);
-  heading_interpolate_status->imu_stamp_buffer.push_back(imu.header.stamp.toSec());
-  imu_stamp_buffer_length = std::distance(heading_interpolate_status->imu_stamp_buffer.begin(), heading_interpolate_status->imu_stamp_buffer.end());
-
-  if (imu_stamp_buffer_length > heading_interpolate_parameter.number_buffer_max)
-  {
-    heading_interpolate_status->provisional_heading_angle_buffer.erase(heading_interpolate_status->provisional_heading_angle_buffer .begin());
-    heading_interpolate_status->imu_stamp_buffer.erase(heading_interpolate_status->imu_stamp_buffer .begin());
-  }
+  provisional_heading_angle_buffer.push_back(provisional_heading_angle);
+  imu_stamp_buffer.push_back(msg->header.stamp.toSec());
 
   if (heading_estimate_start_status == true)
   {
     if (heading_estimate_status == true)
     {
-      for (estimate_index = heading_interpolate_status->number_buffer; estimate_index > 0; estimate_index--)
+      for (estimate_index = number_buffer; estimate_index > 0; estimate_index--)
       {
-        if (heading_interpolate_status->imu_stamp_buffer[estimate_index-1] == heading.header.stamp.toSec())
+        if (imu_stamp_buffer[estimate_index-1] == heading.header.stamp.toSec())
         {
           break;
         }
       }
     }
 
-    if (heading_estimate_status == true && estimate_index > 0 && heading_interpolate_status->number_buffer >= estimate_index && heading_interpolate_status->heading_estimate_status_count > 1)
+    if (heading_estimate_status == true && estimate_index > 0 && number_buffer >= estimate_index && heading_estimate_status_count > 1)
     {
-      diff_estimate_heading_angle = (heading_interpolate_status->provisional_heading_angle_buffer[estimate_index-1] - heading.heading_angle);
-      for (i = estimate_index; i <= heading_interpolate_status->number_buffer; i++)
+      diff_estimate_heading_angle = (provisional_heading_angle_buffer[estimate_index-1] - heading.heading_angle);
+      for (i = estimate_index; i <= number_buffer; i++)
       {
-        heading_interpolate_status->provisional_heading_angle_buffer[i-1] = heading_interpolate_status->provisional_heading_angle_buffer[i-1] - diff_estimate_heading_angle;
+        provisional_heading_angle_buffer[i-1] = provisional_heading_angle_buffer[i-1] - diff_estimate_heading_angle;
       }
-      heading_interpolate_status->provisional_heading_angle = heading_interpolate_status->provisional_heading_angle_buffer[heading_interpolate_status->number_buffer-1];
+      provisional_heading_angle = provisional_heading_angle_buffer[number_buffer-1];
 
-      heading_interpolate->status.enabled_status = true;
-      heading_interpolate->status.estimate_status = true;
+      heading_interpolate.status.enabled_status = true;
+      heading_interpolate.status.estimate_status = true;
     }
-    else if (heading_interpolate_status->heading_estimate_status_count == 1)
+    else if (heading_estimate_status_count == 1)
     {
-      heading_interpolate_status->provisional_heading_angle = heading.heading_angle;
-      heading_interpolate->status.enabled_status = true;
-      heading_interpolate->status.estimate_status = false;
+      provisional_heading_angle = heading.heading_angle;
+      heading_interpolate.status.enabled_status = true;
+      heading_interpolate.status.estimate_status = false;
     }
-    else
+    else if (count > 1)
     {
-      heading_interpolate->status.estimate_status = false;
+      heading_interpolate.status.estimate_status = false;
     }
   }
+
+  /*
+  // angle reversal processing (-3.14~3.14)
+  if (provisional_heading_angle > M_PI)
+  {
+    provisional_heading_angle = provisional_heading_angle - 2.0 * M_PI;
+  }
+  else if (provisional_heading_angle < -M_PI)
+  {
+    provisional_heading_angle = provisional_heading_angle + 2.0 * M_PI;
+  }
+  */
 
   if (heading_estimate_start_status == true)
   {
-    heading_interpolate->heading_angle = heading_interpolate_status->provisional_heading_angle + slip_angle.slip_angle;
+    heading_interpolate.heading_angle = provisional_heading_angle + slip_angle.slip_angle;
   }
   else
   {
-    heading_interpolate->heading_angle = 0.0;
-    heading_interpolate->status.enabled_status = false;
-    heading_interpolate->status.estimate_status = false;
+    heading_interpolate.heading_angle = 0.0;
+    heading_interpolate.status.enabled_status = false;
+    heading_interpolate.status.estimate_status = false;
   }
 
-  heading_interpolate_status->time_last = imu.header.stamp.toSec();
-  heading_interpolate_status->heading_stamp_last = heading.header.stamp.toSec();
+  pub.publish(heading_interpolate);
 
+  time_last = msg->header.stamp.toSec();
+  heading_stamp_last = heading.header.stamp.toSec();
+
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "heading_interpolate");
+  ros::NodeHandle n("~");
+
+  n.getParam("/eagleye/reverse_imu", reverse_imu);
+  n.getParam("/eagleye/heading_interpolate/stop_judgment_velocity_threshold", stop_judgment_velocity_threshold);
+  std::cout<< "reverse_imu "<<reverse_imu<<std::endl;
+  std::cout<< "stop_judgment_velocity_threshold "<<stop_judgment_velocity_threshold<<std::endl;
+
+  std::string publish_topic_name = "/publish_topic_name/invalid";
+  std::string subscribe_topic_name_1 = "/subscribe_topic_name/invalid_1";
+  std::string subscribe_topic_name_2 = "/subscribe_topic_name/invalid_2";
+
+  if (argc == 2)
+  {
+    if (strcmp(argv[1], "1st") == 0)
+    {
+      publish_topic_name = "/eagleye/heading_interpolate_1st";
+      subscribe_topic_name_1 = "/eagleye/yawrate_offset_stop";
+      subscribe_topic_name_2 = "/eagleye/heading_1st";
+    }
+    else if (strcmp(argv[1], "2nd") == 0)
+    {
+      publish_topic_name = "/eagleye/heading_interpolate_2nd";
+      subscribe_topic_name_1 = "/eagleye/yawrate_offset_1st";
+      subscribe_topic_name_2 = "/eagleye/heading_2nd";
+    }
+    else if (strcmp(argv[1], "3rd") == 0)
+    {
+      publish_topic_name = "/eagleye/heading_interpolate_3rd";
+      subscribe_topic_name_1 = "/eagleye/yawrate_offset_2nd";
+      subscribe_topic_name_2 = "/eagleye/heading_3rd";
+    }
+    else
+    {
+      ROS_ERROR("Invalid argument");
+      ros::shutdown();
+    }
+  }
+  else
+  {
+    ROS_ERROR("No arguments");
+    ros::shutdown();
+  }
+
+  ros::Subscriber sub1 = n.subscribe("/imu/data_raw", 1000, imu_callback, ros::TransportHints().tcpNoDelay());
+  ros::Subscriber sub2 = n.subscribe("/eagleye/velocity_scale_factor", 1000, velocity_scale_factor_callback, ros::TransportHints().tcpNoDelay());
+  ros::Subscriber sub3 = n.subscribe("/eagleye/yawrate_offset_stop", 1000, yawrate_offset_stop_callback, ros::TransportHints().tcpNoDelay());
+  ros::Subscriber sub4 = n.subscribe(subscribe_topic_name_1, 1000, yawrate_offset_callback, ros::TransportHints().tcpNoDelay());
+  ros::Subscriber sub5 = n.subscribe(subscribe_topic_name_2, 1000, heading_callback, ros::TransportHints().tcpNoDelay());
+  ros::Subscriber sub6 = n.subscribe("/eagleye/slip_angle", 1000, slip_angle_callback, ros::TransportHints().tcpNoDelay());
+  pub = n.advertise<eagleye_msgs::Heading>(publish_topic_name, 1000);
+
+  ros::spin();
+
+  return 0;
 }
--- eagleye_core/navigation/src/smoothing.cpp
+++ eagleye_core/navigation/src/smoothing.cpp
@@ -1,88 +1,109 @@
-// Copyright (c) 2019, Map IV, Inc.
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// * Redistributions of source code must retain the above copyright notice,
-//   this list of conditions and the following disclaimer.
-// * Redistributions in binary form must reproduce the above copyright notice,
-//   this list of conditions and the following disclaimer in the documentation
-//   and/or other materials provided with the distribution.
-// * Neither the name of the Map IV, Inc. nor the names of its contributors
-//   may be used to endorse or promote products derived from this software
-//   without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-/*
- * smoothing.cpp
- * Author MapIV Takanose
- */
-
+#include "ros/ros.h"
+#include "eagleye_msgs/VelocityScaleFactor.h"
+#include "eagleye_msgs/Position.h"
+#include "sensor_msgs/NavSatFix.h"
+#include "geometry_msgs/Vector3Stamped.h"
+#include "rtklib_msgs/RtklibNav.h"
 #include "coordinate.hpp"
-#include "navigation.hpp"
 
-void smoothing_estimate(rtklib_msgs::RtklibNav rtklib_nav, eagleye_msgs::VelocityScaleFactor velocity_scale_factor,SmoothingParameter smoothing_parameter, SmoothingStatus* smoothing_status,eagleye_msgs::Position* gnss_smooth_pos_enu)
+static double estimated_number = 0;
+static double estimated_number_max = 5*5; //GNSS output cycle　* Smoothing time
+static double estimated_velocity_threshold = 10/3.6;
+static double estimated_threshold = 1/10;
+static double ecef_base_pos_x;
+static double ecef_base_pos_y;
+static double ecef_base_pos_z;
+static double last_pos[3];
+static std::size_t index_length;
+static std::size_t time_buffer_length;
+static std::size_t velocity_index_length;
+
+static rtklib_msgs::RtklibNav rtklib_nav;
+static eagleye_msgs::Position enu_absolute_pos,gnss_smooth_pos_enu;
+static eagleye_msgs::VelocityScaleFactor velocity_scale_factor;
+
+static std::vector<double> time_buffer;
+static std::vector<double> enu_pos_x_buffer, enu_pos_y_buffer,  enu_pos_z_buffer;
+static std::vector<double> correction_velocity_buffer;
+
+static ros::Publisher pub;
+
+
+
+void velocity_scale_factor_callback(const eagleye_msgs::VelocityScaleFactor::ConstPtr& msg)
 {
+  velocity_scale_factor.header = msg->header;
+  velocity_scale_factor.scale_factor = msg->scale_factor;
+  velocity_scale_factor.correction_velocity = msg->correction_velocity;
+  velocity_scale_factor.status = msg->status;
+}
+
+void enu_absolute_pos_callback(const eagleye_msgs::Position::ConstPtr& msg)
+{
+  enu_absolute_pos.header = msg->header;
+  enu_absolute_pos.enu_pos = msg->enu_pos;
+  enu_absolute_pos.ecef_base_pos = msg->ecef_base_pos;
+  enu_absolute_pos.status = msg->status;
+}
+
+void rtklib_nav_callback(const rtklib_msgs::RtklibNav::ConstPtr& msg)
+{
+  rtklib_nav.header = msg->header;
+  rtklib_nav.tow = msg->tow;
+  rtklib_nav.ecef_pos = msg->ecef_pos;
+  rtklib_nav.ecef_vel = msg->ecef_vel;
+  rtklib_nav.status = msg->status;
+
+  if(enu_absolute_pos.ecef_base_pos.x == 0 && enu_absolute_pos.ecef_base_pos.y == 0 && enu_absolute_pos.ecef_base_pos.z == 0)
+  {
+    enu_absolute_pos.ecef_base_pos.x = msg->ecef_pos.x;
+    enu_absolute_pos.ecef_base_pos.y = msg->ecef_pos.y;
+    enu_absolute_pos.ecef_base_pos.z = msg->ecef_pos.z;
+  }
 
   double ecef_pos[3];
   double ecef_base_pos[3];
   double enu_pos[3];
-  std::size_t index_length;
-  std::size_t time_buffer_length;
-  std::size_t velocity_index_length;
 
-  if(smoothing_parameter.ecef_base_pos_x == 0 && smoothing_parameter.ecef_base_pos_y == 0 && smoothing_parameter.ecef_base_pos_z == 0)
-  {
-    smoothing_parameter.ecef_base_pos_x = rtklib_nav.ecef_pos.x;
-    smoothing_parameter.ecef_base_pos_y = rtklib_nav.ecef_pos.y;
-    smoothing_parameter.ecef_base_pos_z = rtklib_nav.ecef_pos.z;
-  }
-
-  ecef_pos[0] = rtklib_nav.ecef_pos.x;
-  ecef_pos[1] = rtklib_nav.ecef_pos.y;
-  ecef_pos[2] = rtklib_nav.ecef_pos.z;
-  ecef_base_pos[0] = smoothing_parameter.ecef_base_pos_x;
-  ecef_base_pos[1] = smoothing_parameter.ecef_base_pos_y;
-  ecef_base_pos[2] = smoothing_parameter.ecef_base_pos_z;
+  ecef_pos[0] = msg->ecef_pos.x;
+  ecef_pos[1] = msg->ecef_pos.y;
+  ecef_pos[2] = msg->ecef_pos.z;
+  ecef_base_pos[0] = enu_absolute_pos.ecef_base_pos.x;
+  ecef_base_pos[1] = enu_absolute_pos.ecef_base_pos.y;
+  ecef_base_pos[2] = enu_absolute_pos.ecef_base_pos.z;
 
   xyz2enu(ecef_pos, ecef_base_pos, enu_pos);
 
-  smoothing_status->time_buffer.push_back(rtklib_nav.header.stamp.toSec());
-  smoothing_status->enu_pos_x_buffer.push_back(enu_pos[0]);
-  smoothing_status->enu_pos_y_buffer.push_back(enu_pos[1]);
-  smoothing_status->enu_pos_z_buffer.push_back(enu_pos[2]);
-  smoothing_status->correction_velocity_buffer.push_back(velocity_scale_factor.correction_velocity.linear.x);
+  ROS_INFO("e = %lf , n = %lf , u = %lf",enu_pos[0],enu_pos[1],enu_pos[2]);
 
-  time_buffer_length = std::distance(smoothing_status->time_buffer.begin(), smoothing_status->time_buffer.end());
+  time_buffer.push_back(msg->header.stamp.toSec());
+  enu_pos_x_buffer.push_back(enu_pos[0]);
+  enu_pos_y_buffer.push_back(enu_pos[1]);
+  enu_pos_z_buffer.push_back(enu_pos[2]);
+  correction_velocity_buffer.push_back(velocity_scale_factor.correction_velocity.linear.x);
 
-  if (time_buffer_length > smoothing_parameter.estimated_number_max)
+  time_buffer_length = std::distance(time_buffer.begin(), time_buffer.end());
+
+  if (time_buffer_length > estimated_number_max)
   {
-    smoothing_status->time_buffer.erase(smoothing_status->time_buffer.begin());
-    smoothing_status->enu_pos_x_buffer.erase(smoothing_status->enu_pos_x_buffer.begin());
-    smoothing_status->enu_pos_y_buffer.erase(smoothing_status->enu_pos_y_buffer.begin());
-    smoothing_status->enu_pos_z_buffer.erase(smoothing_status->enu_pos_z_buffer.begin());
-    smoothing_status->correction_velocity_buffer.erase(smoothing_status->correction_velocity_buffer.begin());
+    ROS_INFO("diff_time = %lu",time_buffer_length);
+    time_buffer.erase(time_buffer.begin());
+    enu_pos_x_buffer.erase(enu_pos_x_buffer.begin());
+    enu_pos_y_buffer.erase(enu_pos_y_buffer.begin());
+    enu_pos_z_buffer.erase(enu_pos_z_buffer.begin());
+    correction_velocity_buffer.erase(correction_velocity_buffer.begin());
   }
 
-  if (smoothing_status->estimated_number < smoothing_parameter.estimated_number_max)
+  if (estimated_number < estimated_number_max)
   {
-    ++smoothing_status->estimated_number;
-    gnss_smooth_pos_enu->status.enabled_status = false;
+    ++estimated_number;
+    gnss_smooth_pos_enu.status.enabled_status = false;
   }
   else
   {
-    smoothing_status->estimated_number = smoothing_parameter.estimated_number_max;
-    gnss_smooth_pos_enu->status.enabled_status = true;
+    estimated_number = estimated_number_max;
+    gnss_smooth_pos_enu.status.enabled_status = true;
   }
 
   std::vector<int> velocity_index;
@@ -92,12 +113,12 @@ void smoothing_estimate(rtklib_msgs::RtklibNav rtklib_nav, eagleye_msgs::Velocit
   double sum_gnss_pos[3] = {0};
 
 
-  if (smoothing_status->estimated_number == smoothing_parameter.estimated_number_max)
+  if (estimated_number == estimated_number_max)
   {
-    for (i = 0; i < smoothing_status->estimated_number; i++)
+    for (i = 0; i < estimated_number; i++)
     {
       index.push_back(i);
-      if (smoothing_status->correction_velocity_buffer[i] > smoothing_parameter.estimated_velocity_threshold)
+      if (correction_velocity_buffer[i] > estimated_velocity_threshold)
       {
         velocity_index.push_back(i);
       }
@@ -106,36 +127,67 @@ void smoothing_estimate(rtklib_msgs::RtklibNav rtklib_nav, eagleye_msgs::Velocit
     index_length = std::distance(index.begin(), index.end());
     velocity_index_length = std::distance(velocity_index.begin(), velocity_index.end());
 
+    ROS_INFO("index_length = %lu , velocity_index_length = %lu ",index_length,velocity_index_length);
+
+
     for (i = 0; i < velocity_index_length; i++)
     {
-      sum_gnss_pos[0] = sum_gnss_pos[0] + smoothing_status->enu_pos_x_buffer[velocity_index[i]];
-      sum_gnss_pos[1] = sum_gnss_pos[1] + smoothing_status->enu_pos_y_buffer[velocity_index[i]];
-      sum_gnss_pos[2] = sum_gnss_pos[2] + smoothing_status->enu_pos_z_buffer[velocity_index[i]];
+      sum_gnss_pos[0] = sum_gnss_pos[0] + enu_pos_x_buffer[velocity_index[i]];
+      sum_gnss_pos[1] = sum_gnss_pos[1] + enu_pos_y_buffer[velocity_index[i]];
+      sum_gnss_pos[2] = sum_gnss_pos[2] + enu_pos_z_buffer[velocity_index[i]];
     }
+    ROS_INFO("s_e = %lf , s_n = %lf , s_u = %lf",sum_gnss_pos[0],sum_gnss_pos[1],sum_gnss_pos[2]);
 
-    if (velocity_index_length > index_length * smoothing_parameter.estimated_threshold)
+
+    if (velocity_index_length > index_length * estimated_threshold)
     {
       gnss_smooth_pos[0] = sum_gnss_pos[0]/velocity_index_length;
       gnss_smooth_pos[1] = sum_gnss_pos[1]/velocity_index_length;
       gnss_smooth_pos[2] = sum_gnss_pos[2]/velocity_index_length;
-      gnss_smooth_pos_enu->status.estimate_status = true;
+      gnss_smooth_pos_enu.status.estimate_status = true;
     }
     else
     {
-      gnss_smooth_pos[0] = smoothing_status->last_pos[0];
-      gnss_smooth_pos[1] = smoothing_status->last_pos[1];
-      gnss_smooth_pos[2] = smoothing_status->last_pos[2];
-      gnss_smooth_pos_enu->status.estimate_status = false;
+      gnss_smooth_pos[0] = last_pos[0];
+      gnss_smooth_pos[1] = last_pos[1];
+      gnss_smooth_pos[2] = last_pos[2];
+      gnss_smooth_pos_enu.status.estimate_status = false;
     }
 
-    smoothing_status->last_pos[0] = gnss_smooth_pos[0];
-    smoothing_status->last_pos[1] = gnss_smooth_pos[1];
-    smoothing_status->last_pos[2] = gnss_smooth_pos[2];
+    last_pos[0] = gnss_smooth_pos[0];
+    last_pos[1] = gnss_smooth_pos[1];
+    last_pos[2] = gnss_smooth_pos[2];
   }
 
-  gnss_smooth_pos_enu->enu_pos.x = gnss_smooth_pos[0];
-  gnss_smooth_pos_enu->enu_pos.y = gnss_smooth_pos[1];
-  gnss_smooth_pos_enu->enu_pos.z = gnss_smooth_pos[2];
-  // gnss_smooth_pos_enu->ecef_base_pos = enu_absolute_pos.ecef_base_pos;
-  gnss_smooth_pos_enu->header = rtklib_nav.header;
+  gnss_smooth_pos_enu.enu_pos.x = gnss_smooth_pos[0];
+  gnss_smooth_pos_enu.enu_pos.y = gnss_smooth_pos[1];
+  gnss_smooth_pos_enu.enu_pos.z = gnss_smooth_pos[2];
+  gnss_smooth_pos_enu.ecef_base_pos = enu_absolute_pos.ecef_base_pos;
+  gnss_smooth_pos_enu.header = msg->header;
+  pub.publish(gnss_smooth_pos_enu);
+
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "smoothing");
+  ros::NodeHandle n;
+
+  n.getParam("/eagleye/position/ecef_base_pos_x",ecef_base_pos_x);
+  n.getParam("/eagleye/position/ecef_base_pos_y",ecef_base_pos_y);
+  n.getParam("/eagleye/position/ecef_base_pos_z",ecef_base_pos_z);
+
+  std::cout<< "ecef_base_pos_x "<<ecef_base_pos_x<<std::endl;
+  std::cout<< "ecef_base_pos_y "<<ecef_base_pos_y<<std::endl;
+  std::cout<< "ecef_base_pos_z "<<ecef_base_pos_z<<std::endl;
+
+  ros::Subscriber sub1 = n.subscribe("/eagleye/velocity_scale_factor", 1000, velocity_scale_factor_callback, ros::TransportHints().tcpNoDelay());
+  ros::Subscriber sub2 = n.subscribe("/eagleye/enu_absolute_pos", 1000, enu_absolute_pos_callback, ros::TransportHints().tcpNoDelay());
+  ros::Subscriber sub3 = n.subscribe("/rtklib_nav", 1000, rtklib_nav_callback, ros::TransportHints().tcpNoDelay());
+
+  pub = n.advertise<eagleye_msgs::Position>("/eagleye/gnss_smooth_pos_enu", 1000);
+
+  ros::spin();
+
+  return 0;
 }
