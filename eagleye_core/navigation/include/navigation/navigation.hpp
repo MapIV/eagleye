9436b82426bfae87076198419a4626599638c75c
Ryohei Sasaki <ryohei.sasaki@tier4.jp>
2022-04-07 17:56:23 +0900

    Initialize velocity scale factor

--- eagleye_core/navigation/include/navigation/navigation.hpp
+++ eagleye_core/navigation/include/navigation/navigation.hpp
@@ -68,7 +68,7 @@ struct VelocityScaleFactorStatus
   std::vector<double> velocity_buffer;
   int tow_last, estimated_number;
   double rmc_time_last;
-  double velocity_scale_factor_last;
+  double velocity_scale_factor_last{1.0};
   bool estimate_start_status;
 };
 
