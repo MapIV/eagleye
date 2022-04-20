e4d3d02c204bfb0804706c045feda0bdaefb4656
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2022-04-20 09:40:53 +0900

    eagleye works well for T4 rosbags

--- eagleye_rt/src/ublox_node.cpp
+++ eagleye_rt/src/ublox_node.cpp
@@ -1,9 +1,13 @@
 #include <eagleye_coordinate/eagleye_coordinate.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <rtklib_msgs/msg/rtklib_nav.hpp>
+#include <sensor_msgs/msg/nav_sat_fix.hpp>
 #include <ublox_msgs/msg/nav_pvt.hpp>
 
 rclcpp::Publisher<rtklib_msgs::msg::RtklibNav>::SharedPtr pub_rtk;
+sensor_msgs::msg::NavSatFix nav_msg;
+
+void fix_callback(const sensor_msgs::msg::NavSatFix::ConstSharedPtr msg) { nav_msg = *msg; }
 
 void ublox_callback(const ublox_msgs::msg::NavPVT::ConstSharedPtr msg)
 {
@@ -11,8 +15,8 @@ void ublox_callback(const ublox_msgs::msg::NavPVT::ConstSharedPtr msg)
   r.header.frame_id = "gps";
   r.header.stamp.sec = msg->sec;
   r.header.stamp.nanosec = msg->nano;
-  // r.status;            // sensor_msgs::msg::NavSatFix // これはmoniorに出力されるだけ
-  r.tow = msg->i_tow;  // int64_t<-uint32_t NOTE:
+  r.status = nav_msg;
+  r.tow = msg->i_tow;
 
   double llh[3];
   llh[0] = msg->lat * 1e-7;
@@ -37,9 +41,11 @@ int main(int argc, char** argv)
   auto node = rclcpp::Node::make_shared("ublox");
 
   std::string subscribe_ublox_topic_name = "/sensing/gnss/ublox/navpvt";
+  std::string subscribe_fix_topic_name = "/sensing/gnss/ublox/nav_sat_fix";
   std::string publish_rtklib_topic_name = "/rtklib_nav";
 
-  auto sub = node->create_subscription<ublox_msgs::msg::NavPVT>(subscribe_ublox_topic_name, 1000, ublox_callback);
+  auto sub_ublox = node->create_subscription<ublox_msgs::msg::NavPVT>(subscribe_ublox_topic_name, 1000, ublox_callback);
+  auto sub_fix = node->create_subscription<sensor_msgs::msg::NavSatFix>(subscribe_fix_topic_name, 1000, fix_callback);
   pub_rtk = node->create_publisher<rtklib_msgs::msg::RtklibNav>(publish_rtklib_topic_name, 10);
 
   rclcpp::spin(node);
