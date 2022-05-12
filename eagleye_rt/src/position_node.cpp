f82fc70636a747e1d16bb9e6d144bb4ce0fd983f
Ryohei Sasaki <rsasaki0109@gmail.com>
2022-05-12 11:36:57 +0900

    Fix name

--- eagleye_rt/src/position_node.cpp
+++ eagleye_rt/src/position_node.cpp
@@ -74,20 +74,20 @@ void gga_callback(const nmea_msgs::Gpgga::ConstPtr& msg)
   _gga = *msg;
 }
 
-void timer_callback(const ros::TimerEvent& e, tf2_ros::TransformListener* tfListener_, tf2_ros::Buffer* tfBuffer_)
+void timer_callback(const ros::TimerEvent& e, tf2_ros::TransformListener* tf_listener, tf2_ros::Buffer* tf_buffer)
 {
-  geometry_msgs::TransformStamped transformStamped;
+  geometry_msgs::TransformStamped transform_stamped;
   try
   {
-    transformStamped = tfBuffer_->lookupTransform(_position_parameter.tf_gnss_parent_frame, _position_parameter.tf_gnss_child_frame, ros::Time(0));
+    transform_stamped = tf_buffer->lookupTransform(_position_parameter.tf_gnss_parent_frame, _position_parameter.tf_gnss_child_frame, ros::Time(0));
 
-    _position_parameter.tf_gnss_translation_x = transformStamped.transform.translation.x;
-    _position_parameter.tf_gnss_translation_y = transformStamped.transform.translation.y;
-    _position_parameter.tf_gnss_translation_z = transformStamped.transform.translation.z;
-    _position_parameter.tf_gnss_rotation_x = transformStamped.transform.rotation.x;
-    _position_parameter.tf_gnss_rotation_y = transformStamped.transform.rotation.y;
-    _position_parameter.tf_gnss_rotation_z = transformStamped.transform.rotation.z;
-    _position_parameter.tf_gnss_rotation_w = transformStamped.transform.rotation.w;
+    _position_parameter.tf_gnss_translation_x = transform_stamped.transform.translation.x;
+    _position_parameter.tf_gnss_translation_y = transform_stamped.transform.translation.y;
+    _position_parameter.tf_gnss_translation_z = transform_stamped.transform.translation.z;
+    _position_parameter.tf_gnss_rotation_x = transform_stamped.transform.rotation.x;
+    _position_parameter.tf_gnss_rotation_y = transform_stamped.transform.rotation.y;
+    _position_parameter.tf_gnss_rotation_z = transform_stamped.transform.rotation.z;
+    _position_parameter.tf_gnss_rotation_w = transform_stamped.transform.rotation.w;
   }
   catch (tf2::TransformException& ex)
   {
@@ -160,9 +160,9 @@ int main(int argc, char** argv)
   
   _pub = nh.advertise<eagleye_msgs::Position>("enu_absolute_pos", 1000);
 
-  tf2_ros::Buffer tfBuffer_;
-  tf2_ros::TransformListener tfListener_(tfBuffer_);
-  ros::Timer timer = nh.createTimer(ros::Duration(0.5), boost::bind(timer_callback,_1, &tfListener_, &tfBuffer_));
+  tf2_ros::Buffer tf_buffer;
+  tf2_ros::TransformListener tf_listener(tf_buffer);
+  ros::Timer timer = nh.createTimer(ros::Duration(0.5), boost::bind(timer_callback,_1, &tf_listener, &tf_buffer));
 
   ros::spin();
 
