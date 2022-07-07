503388544fa128e0b1be34b2bf97d96521777e09
Ryohei Sasaki <ryohei.sasaki@tier4.jp>
2022-07-07 17:43:50 +0900

    Add processing when input twist does not come in for a certain amount of time (#146)

--- eagleye_rt/src/trajectory_node.cpp
+++ eagleye_rt/src/trajectory_node.cpp
@@ -53,6 +53,12 @@ rclcpp::Publisher<geometry_msgs::msg::TwistWithCovarianceStamped>::SharedPtr pub
 struct TrajectoryParameter trajectory_parameter;
 struct TrajectoryStatus trajectory_status;
 
+static double timer_updata_rate = 10;
+static double th_deadlock_time = 1;
+
+static double imu_time_last,velocity_time_last;
+static bool input_status;
+
 static bool use_canless_mode;
 
 void correction_velocity_callback(const geometry_msgs::msg::TwistStamped::ConstSharedPtr msg)
@@ -95,6 +101,27 @@ void velocity_callback(const geometry_msgs::msg::TwistStamped::ConstSharedPtr ms
   velocity = *msg;
 }
 
+void on_timer()
+{
+  rclcpp::Time imu_clock(imu.header.stamp);
+  double imu_time = imu_clock.seconds();
+  rclcpp::Time velocity_clock(velocity.header.stamp);
+  double velocity_time = velocity_clock.seconds();
+  if (std::abs(imu_time - imu_time_last) < th_deadlock_time &&
+      std::abs(velocity_time - velocity_time_last) < th_deadlock_time &&
+      std::abs(velocity_time - imu_time) < th_deadlock_time)
+  {
+    input_status = true;
+  }
+  else
+  {
+    input_status = false;
+    RCLCPP_WARN(rclcpp::get_logger("trajectory"), "Twist is missing the required input topics.");
+  }
+
+  if (imu_time != imu_time_last) imu_time_last = imu_time;
+  if (velocity_time != velocity_time_last) velocity_time_last = velocity_time;
+}
 
 void imu_callback(const sensor_msgs::msg::Imu::ConstSharedPtr msg)
 {
@@ -112,36 +139,39 @@ void imu_callback(const sensor_msgs::msg::Imu::ConstSharedPtr msg)
   }
 
   imu = *msg;
-  enu_vel.header = msg->header;
-  enu_vel.header.frame_id = "gnss";
-  enu_relative_pos.header = msg->header;
-  enu_relative_pos.header.frame_id = "base_link";
-  eagleye_twist.header = msg->header;
-  eagleye_twist.header.frame_id = "base_link";
-  trajectory3d_estimate(imu,correction_velocity,velocity_enable_status,heading_interpolate_3rd,yawrate_offset_stop,yawrate_offset_2nd,pitching,
-    trajectory_parameter,&trajectory_status,&enu_vel,&enu_relative_pos,&eagleye_twist);
-
-  if (heading_interpolate_3rd.status.enabled_status == true)
+  if(input_status)
   {
-    pub1->publish(enu_vel);
-    pub2->publish(enu_relative_pos);
+    enu_vel.header = msg->header;
+    enu_vel.header.frame_id = "gnss";
+    enu_relative_pos.header = msg->header;
+    enu_relative_pos.header.frame_id = "base_link";
+    eagleye_twist.header = msg->header;
+    eagleye_twist.header.frame_id = "base_link";
+    trajectory3d_estimate(imu,correction_velocity,velocity_enable_status,heading_interpolate_3rd,yawrate_offset_stop,yawrate_offset_2nd,pitching,
+      trajectory_parameter,&trajectory_status,&enu_vel,&enu_relative_pos,&eagleye_twist);
+
+    if (heading_interpolate_3rd.status.enabled_status)
+    {
+      pub1->publish(enu_vel);
+      pub2->publish(enu_relative_pos);
+    }
+    pub3->publish(eagleye_twist);
+
+    geometry_msgs::msg::TwistWithCovarianceStamped eagleye_twist_with_covariance;
+    eagleye_twist_with_covariance.header = msg->header;
+    eagleye_twist_with_covariance.header.frame_id = "base_link";
+    eagleye_twist_with_covariance.twist.twist = eagleye_twist.twist;
+    // TODO(Map IV): temporary value
+    // linear.y, linear.z, angular.x, and angular.y are not calculated values.
+    eagleye_twist_with_covariance.twist.covariance[0] = 0.2 * 0.2;
+    eagleye_twist_with_covariance.twist.covariance[7] = 10000.0;
+    eagleye_twist_with_covariance.twist.covariance[14] = 10000.0;
+    eagleye_twist_with_covariance.twist.covariance[21] = 10000.0;
+    eagleye_twist_with_covariance.twist.covariance[28] = 10000.0;
+    eagleye_twist_with_covariance.twist.covariance[35] = 0.1 * 0.1;
+
+    pub4->publish(eagleye_twist_with_covariance);
   }
-  pub3->publish(eagleye_twist);
-
-  geometry_msgs::msg::TwistWithCovarianceStamped eagleye_twist_with_covariance;
-  eagleye_twist_with_covariance.header = msg->header;
-  eagleye_twist_with_covariance.header.frame_id = "base_link";
-  eagleye_twist_with_covariance.twist.twist = eagleye_twist.twist;
-  // TODO(Map IV): temporary value
-  // linear.y, linear.z, angular.x, and angular.y are not calculated values.
-  eagleye_twist_with_covariance.twist.covariance[0] = 0.2 * 0.2;
-  eagleye_twist_with_covariance.twist.covariance[7] = 10000.0;
-  eagleye_twist_with_covariance.twist.covariance[14] = 10000.0;
-  eagleye_twist_with_covariance.twist.covariance[21] = 10000.0;
-  eagleye_twist_with_covariance.twist.covariance[28] = 10000.0;
-  eagleye_twist_with_covariance.twist.covariance[35] = 0.1 * 0.1;
-
-  pub4->publish(eagleye_twist_with_covariance);
 }
 
 int main(int argc, char** argv)
@@ -155,17 +185,24 @@ int main(int argc, char** argv)
   node->declare_parameter("reverse_imu", trajectory_parameter.reverse_imu);
   node->declare_parameter("trajectory.stop_judgment_velocity_threshold",trajectory_parameter.stop_judgment_velocity_threshold);
   node->declare_parameter("trajectory.stop_judgment_yawrate_threshold",trajectory_parameter.stop_judgment_yawrate_threshold);
+  node->declare_parameter("timer_updata_rate",timer_updata_rate);
+  node->declare_parameter("th_deadlock_time",th_deadlock_time);
   node->declare_parameter("use_canless_mode",use_canless_mode);
 
   node->get_parameter("imu_topic",subscribe_imu_topic_name);
   node->get_parameter("reverse_imu", trajectory_parameter.reverse_imu);
   node->get_parameter("trajectory.stop_judgment_velocity_threshold",trajectory_parameter.stop_judgment_velocity_threshold);
   node->get_parameter("trajectory.stop_judgment_yawrate_threshold",trajectory_parameter.stop_judgment_yawrate_threshold);
+  node->get_parameter("timer_updata_rate",timer_updata_rate);
+  node->get_parameter("th_deadlock_time",th_deadlock_time);
   node->get_parameter("use_canless_mode",use_canless_mode);
+
   std::cout<< "subscribe_imu_topic_name "<<subscribe_imu_topic_name<<std::endl;
   std::cout<< "reverse_imu "<<trajectory_parameter.reverse_imu<<std::endl;
   std::cout<< "stop_judgment_velocity_threshold "<<trajectory_parameter.stop_judgment_velocity_threshold<<std::endl;
   std::cout<< "stop_judgment_yawrate_threshold "<<trajectory_parameter.stop_judgment_yawrate_threshold<<std::endl;
+  std::cout<< "timer_updata_rate "<<timer_updata_rate<<std::endl;
+  std::cout<< "th_deadlock_time "<<th_deadlock_time<<std::endl;
   std::cout<< "use_canless_mode "<<use_canless_mode<<std::endl;
 
   auto sub1 = node->create_subscription<sensor_msgs::msg::Imu>(subscribe_imu_topic_name, 1000, imu_callback);  //ros::TransportHints().tcpNoDelay()
@@ -180,6 +217,15 @@ int main(int argc, char** argv)
   pub2 = node->create_publisher<eagleye_msgs::msg::Position>("enu_relative_pos", 1000);
   pub3 = node->create_publisher<geometry_msgs::msg::TwistStamped>("twist", 1000);
   pub4 = node->create_publisher<geometry_msgs::msg::TwistWithCovarianceStamped>("twist_with_covariance", 1000);
+
+  double delta_time = 1.0 / static_cast<double>(timer_updata_rate);
+  auto timer_callback = std::bind(on_timer);
+  const auto period_ns =
+    std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::duration<double>(delta_time));
+  auto timer = std::make_shared<rclcpp::GenericTimer<decltype(timer_callback)>>(
+    node->get_clock(), period_ns, std::move(timer_callback),
+    node->get_node_base_interface()->get_context());
+  node->get_node_timers_interface()->add_timer(timer, nullptr);
 
   rclcpp::spin(node);
 
