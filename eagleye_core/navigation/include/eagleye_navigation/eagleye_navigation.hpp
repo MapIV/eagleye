d3f8e098e23e341421ce2b54d120c30dbd3c3f97
Ryohei Sasaki <ryohei.sasaki@tier4.jp>
2022-12-01 10:31:11 +0900

    Add dependency in gnss_converter package.xml

--- eagleye_core/navigation/include/eagleye_navigation/eagleye_navigation.hpp
+++ eagleye_core/navigation/include/eagleye_navigation/eagleye_navigation.hpp
@@ -54,7 +54,7 @@
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <math.h>
 #include <numeric>
 
--- eagleye_util/fix2pose/src/fix2pose.cpp
+++ eagleye_util/fix2pose/src/fix2pose.cpp
@@ -38,7 +38,7 @@
 #include "eagleye_msgs/msg/heading.hpp"
 #include "eagleye_msgs/msg/position.hpp"
 #include "tf2_ros/transform_broadcaster.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 #include "eagleye_coordinate/eagleye_coordinate.hpp"
 
 static eagleye_msgs::msg::Rolling eagleye_rolling;
