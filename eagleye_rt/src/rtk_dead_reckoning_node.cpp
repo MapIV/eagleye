7deb505129314ac8f5ae1414c17c432ce79b5afa
Yukihiro Saito <yukky.saito@gmail.com>
2023-05-18 18:53:03 +0900

    cosmetic change
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>

--- eagleye_rt/src/rtk_dead_reckoning_node.cpp
+++ eagleye_rt/src/rtk_dead_reckoning_node.cpp
@@ -34,7 +34,7 @@
 
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
-##ifdef ROS_DISTRO_GALACTIC
+#ifdef ROS_DISTRO_GALACTIC
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #else
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
